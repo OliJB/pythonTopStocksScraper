56ed8b0862831947e6f442c72612caaf6885b81e
Oliver Bradbury <oliver.bradbury@icloud.com>
2024-12-15 19:58:08 +0000

    Quick code cleanup

--- main.py
+++ main.py
@@ -1,30 +1,29 @@
 from selenium import webdriver
 from selenium.webdriver.common.by import By
 
-
-url = 'https://www.monster.co.uk/jobs/search?q=software+engineer&where=London%2C+England&page=1&so=m.h.lh'
-url_2 = 'https://www.python.org/'
-url_3 = 'https://finance.yahoo.com/markets/stocks/gainers/'
+#define url and webdriver
+url = 'https://finance.yahoo.com/markets/stocks/gainers/'
 driver = webdriver.Firefox()
 
-driver.get(url_3)
-
+#start the scrape, opening the website & ensure time for loading
+driver.get(url)
 driver.implicitly_wait(10)
 
+#get past the cookies acceptance
 driver.find_element(By.ID,'scroll-down-btn').click()
 accept_button = driver.find_element(By.CLASS_NAME, 'btn.secondary.accept-all')
 accept_button.click()
-
 
 try:
     # Find all rows with the specific class name
     rows = driver.find_elements(By.CLASS_NAME, "row.false.yf-paf8n5")
 
-    # Loop through each row and print its text
+    # Loop through each row and print the scraped data
     for index, row in enumerate(rows):
         print(f"Row {index + 1}:")
-        print(row.text)  # Extract and print all text content in the row
-        print("-" * 50)  # Separator for better readability
+        print(row.text)
+        print("-" * 50)
 
+#try except for error handling
 except Exception as e:
     print(f"An error occurred: {e}")
